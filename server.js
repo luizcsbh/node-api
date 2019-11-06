99d4c7c15fa61609775ba70a002276c916ce5e27
Luiz Santos <luizcsbh@gmail.com>
2019-11-06 14:47:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 //Iniciando o DB
-mongoose.connect("mongodb+srv://mongodb:mongodbapp@clusternode-api-eiabw.mongodb.net/test?retryWrites=true&w=majority/nodeapi", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://mongodb:3MLMPykuDfiRjrHH@clusternode-api-eiabw.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 
 requireDir('./src/models');
 
